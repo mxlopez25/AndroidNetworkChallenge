64039856681fdd05693f46881a602266ce9c667c
Maximo Lopez <=>
2022-02-05 09:32:33 -0400

    Initial Commit

---
+++ app/src/main/java/com/mxlopez/androidnetworkchallange/models/LocationModel.kt
@@ -0,0 +1,3 @@
+package com.mxlopez.androidnetworkchallange.models
+
+data class LocationModel(val name: String, val url: String?)
---
+++ app/src/main/java/com/mxlopez/androidnetworkchallange/models/OriginModel.kt
@@ -0,0 +1,3 @@
+package com.mxlopez.androidnetworkchallange.models
+
+data class OriginModel(val name: String, val url: String?)
