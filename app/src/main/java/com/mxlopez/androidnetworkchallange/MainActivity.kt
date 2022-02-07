1ab8ca60364721de6cde9c1b18d4a954eda84744
Maximo Lopez <=>
2022-02-07 01:35:15 -0400

    Update on List structure, removed comments and removed images

--- app/src/main/java/com/mxlopez/androidnetworkchallange/MainActivity.kt
+++ app/src/main/java/com/mxlopez/androidnetworkchallange/MainActivity.kt
@@ -3,22 +3,27 @@ package com.mxlopez.androidnetworkchallange
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.res.TypedArrayUtils.getText
 import androidx.lifecycle.ViewModelProvider
 import coil.compose.rememberImagePainter
 import com.mxlopez.androidnetworkchallange.models.CharacterModel
@@ -39,80 +44,54 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainComponent(vm: CharactersViewModel, context: Context) {
-            LaunchedEffect(Unit, block = {
-                EspressoIdlingResource.increment()
-                vm.getCharacterList(context)
-            })
+    LaunchedEffect(Unit, block = {
+        EspressoIdlingResource.increment()
+        vm.getCharacterList(context)
+    })
 
-            Scaffold(
-                topBar = {
-                    TopAppBar() {
-                        Text(text = "Characters")
-                    }
-                },
-                content = {
-                    Column(modifier = Modifier.padding(16.dp)) {
-                        LazyColumn(modifier = Modifier.fillMaxHeight()) {
-                            items(vm.characterList) { character ->
-                                Helper.setCharacter(character)
-                                Column {
-//                                    if(character.status == "alive") {
-                                        Row (modifier = Modifier
-                                            .fillMaxWidth()
-                                            .padding(16.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-                                            Box(modifier = Modifier
-                                                .fillMaxWidth()
-                                                .padding(0.dp, 0.dp, 16.dp, 0.dp)) {
-                                                Row {
-                                                    Column {
-                                                        Card(modifier = Modifier
-                                                            .size(70.dp)
-                                                            .padding(5.dp), backgroundColor = Color.Black) {
-                                                            Image(
-                                                                painter = rememberImagePainter(character.image),
-                                                                contentDescription = "Character Avatar",
-                                                                modifier = Modifier.fillMaxSize()
-                                                            )
-                                                        }
-                                                    }
-                                                    Column {
-                                                        Row() {
-                                                            Text("Name: ", fontWeight = FontWeight.Bold)
-                                                            Text(character.name)
-                                                        }
-                                                        Row() {
-                                                            Text(text = "Species: ", fontWeight = FontWeight.Bold)
-                                                            Text(character.species)
-                                                        }
-                                                    }
-                                                    Column {
-                                                        Button(
-                                                            onClick = {
-                                                                Log.d(
-                                                                    "ButtonTest",
-                                                                    character.name
-                                                                )
-                                                            },
-                                                            modifier = Modifier
-                                                                .fillMaxWidth()
-                                                                .fillMaxHeight()
-                                                        ) {
-                                                            Text(text = "Click here for test")
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                            Spacer(modifier = Modifier.width(16.dp))
-
-                                        }
-//                                    }
-                                }
-                            }
+    Scaffold(
+        topBar = {
+            TopAppBar() {
+                Text(text = "Characters")
+            }
+        }
+    ) {
+        LazyColumn (modifier = Modifier.fillMaxWidth()) {
+            items(vm.characterList) { character ->
+                Helper.setCharacter(character)
+                Row(modifier = Modifier.fillMaxWidth().clickable { Toast.makeText(
+                    context,
+                    "Selected: ${character.name}",
+                    Toast.LENGTH_SHORT
+                ).show() }) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.size(100.dp, 80.dp)) {
+                        Card(
+                            modifier = Modifier
+                                .padding(5.dp)
+                                .size(75.dp, 75.dp),
+                            backgroundColor = Color.Black
+                        ) {
+                            Image(
+                                painter = rememberImagePainter(character.image),
+                                contentDescription = "Character Avatar"
+                            )
                         }
                     }
+                    Column(modifier = Modifier.size(250.dp, 75.dp)) {
+                        Text(
+                            text = "Name: ${character.name}",
+                            fontWeight = FontWeight.Bold,
+                            overflow = TextOverflow.Clip
+                        )
+                        Text(text = "Species: ${character.species}", fontWeight = FontWeight.Bold)
+                    }
                 }
-            )
+                Spacer(modifier = Modifier.padding(0.dp, 16.dp, 0.dp, 0.dp))
+            }
+        }
+    }
 }
+
 
 
 
